63334b5c654dc0f890f63a9e14f77a536396445e
525santushevich <sophiaantushevich@icloud.com>
2024-06-07 10:42:31 -0400

    final js commit

--- script.js
+++ script.js
@@ -1,144 +1,99 @@
 const teamMembers = [
   {
     name: 'Naomi Campbell',
-    img: MediaSession.jpg,
-    age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 54,
+    retired: false,
+    brands: ['Versace', 'Valentino', 'Chanel'],
+    bestQuality: 'Catwalk presence',
+    biography: 'Naomi Campbell is a British supermodel, actress, and businesswoman. She began her career at the age of 15 and has become one of the most recognizable and in-demand models of the past four decades.'
   },
   {
     name: 'Cindy Crawford',
-    position: 'Defender',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 58,
+    retired: true,
+    brands: ['Pepsi', 'Revlon', 'Versace'],
+    bestQuality: 'Signature mole',
+    biography: 'Cynthia Ann Crawford is an American model, actress, and television personality. Her years of success at modeling made her an international celebrity that has led to roles in television and film, and work as a spokesperson.'
   },
   {
     name: 'Adriana Lima',
-    img: MediaSession.jpg,
     age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    retired: false,
+    brands: ['Victoria\'s Secret', 'Maybelline', 'Miu Miu'],
+    bestQuality: 'Intense gaze',
+    biography: 'Adriana Lima is a Brazilian model and actress, best known as a Victoria\'s Secret Angel. She has been the longest-running model for the brand and was named "the most valuable Victoria\'s Secret Angel" in 2017.'
   },
   {
     name: 'Gisele Bundchen',
-    img: MediaSession.jpg,
-    age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 43,
+    retired: true,
+    brands: ['Victoria\'s Secret', 'Chanel', 'Versace'],
+    bestQuality: 'Natural beauty',
+    biography: 'Gisele Bündchen is a Brazilian model and actress, often referred to as one of the greatest supermodels of all time. She has been one of the highest-paid models in the world and is a former Victoria\'s Secret Angel.'
   },
   {
     name: 'Yasmeen Ghauri',
-    img: MediaSession.jpg,
-    age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 52,
+    retired: true,
+    brands: ['Hermes', 'Lanvin', 'Yves Saint Laurent'],
+    bestQuality: 'Elegant walk',
+    biography: 'Yasmeen Ghauri is a Canadian former supermodel. She is known for her work with designers like Hermès and Lanvin. Ghauri retired from modeling to focus on her family and personal life.'
   },
   {
     name: 'Janice Dickinson',
-    img: MediaSession.jpg,
-    age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 68,
+    retired: true,
+    brands: ['Revlon', 'Vogue', 'Chanel'],
+    bestQuality: 'Bold personality',
+    biography: 'Janice Dickinson is an American model, television personality, and author. She has been described as the first supermodel and has appeared on the covers of numerous fashion magazines.'
   },
   {
     name: 'Claudia Schiffer',
-    img: MediaSession.jpg,
-    age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 53,
+    retired: true,
+    brands: ['Chanel', 'Versace', 'Dior'],
+    bestQuality: 'Classic beauty',
+    biography: 'Claudia Schiffer is a German model and actress, considered one of the most successful models of all time. She has appeared on over 1,000 magazine covers and was one of the original supermodels of the 1990s.'
   },
   {
     name: 'Shalom Harlow',
-    img: MediaSession.jpg,
-    age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 50,
+    retired: false,
+    brands: ['Alexander McQueen', 'Versace', 'Victoria\'s Secret'],
+    bestQuality: 'Versatility',
+    biography: 'Shalom Harlow is a Canadian model and actress. She gained fame in the early 1990s and is known for her versatility and ability to transition between high fashion and commercial modeling.'
   },
   {
     name: 'Heidi Klum',
-    img: MediaSession.jpg,
-    age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 50,
+    retired: false,
+    brands: ['Victoria\'s Secret', 'Sports Illustrated', 'Givenchy'],
+    bestQuality: 'Charisma',
+    biography: 'Heidi Klum is a German-American model, television host, producer, and businesswoman. She became internationally famous as a Victoria\'s Secret Angel and for appearing on the cover of the Sports Illustrated Swimsuit Issue.'
   },
   {
     name: 'Kendall Jenner',
-    img: MediaSession.jpg,
-    age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 28,
+    retired: false,
+    brands: ['Calvin Klein', 'Estée Lauder', 'Givenchy'],
+    bestQuality: 'Social media presence',
+    biography: 'Kendall Jenner is an American model and media personality. She is a member of the Kardashian-Jenner family and gained fame through the reality TV show "Keeping Up with the Kardashians."'
   },
   {
     name: 'Cara Delevingne',
-    img: MediaSession.jpg,
-    age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 31,
+    retired: false,
+    brands: ['Burberry', 'Chanel', 'Yves Saint Laurent'],
+    bestQuality: 'Eyebrows',
+    biography: 'Cara Delevingne is an English model, actress, and singer. She is known for her distinctive eyebrows and has appeared in numerous fashion campaigns, as well as acting in several films.'
   },
   {
     name: 'Taylor Hill',
-    img: MediaSession.jpg,
-    age: 36,
-    activelyPlaying: true,
-    position: 'Forward',
-    skills: ['Dribbling', 'Shooting', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography:
-      'Lionel Messi is considered one of the greatest football players of all time...'
+    age: 28,
+    retired: false,
+    brands: ['Victoria\'s Secret', 'Versace', 'Michael Kors'],
+    bestQuality: 'All-American looks',
+    biography: 'Taylor Hill is an American model and actress. She is best known for being a Victoria\'s Secret Angel and has walked the runway for numerous high-profile fashion brands.'
   }
 ]
 
@@ -149,50 +104,30 @@ function generateTeamCards() {
     const card = document.createElement('div')
     card.classList.add('col-md-4')
 
-    //styling card based on position:
-    let backgroundColor
-
-    switch (member.position.toLowerCase()) {
-      case 'forward':
-        backgroundColor = '#FFB6C1' // Yellow for forwards
-        break
-      case 'midfielder':
-        backgroundColor = '#28a745' // Green for midfielders
-        break
-      case 'defender':
-        backgroundColor = 'pic' // Blue for defenders
-        break
-      case 'goalkeeper':
-        backgroundColor = '#dc3545' // Red for goalkeepers
-        break
-      default:
-        backgroundColor = '#6c757d' // Gray for other positions
-    }
-    card.style.backgroundColor = backgroundColor
-
-    //Create a list of Skills with <li> tags
-    const skillsList = member.skills
-      .map(skill => `<li> ${skill} </li>`).join('')
+    // Create a list of brands with <li> tags
+    const brandsList = member.brands
+      .map(brand => `<li>${brand}</li>`).join('')
 
     card.innerHTML = `
-            <div class = "card">
-                <div class = "card-header"> ${member.name}</div>
-                <div class = "card-body">
-                    <p><strong>Position:</strong> ${member.position}</p>
-                    <p><strong>Skills:</strong> 
-                        <ul>
-                            ${skillsList}
-                        </ul>
-                    </p>
-                    <p><strong>Strengths:</strong> ${member.strengths}</p>
-                    <p><strong>Weaknesses:</strong> ${member.weaknesses}</p>
-                    <p><strong>Biography:</strong> ${member.biography}</p>
-                </div>
-            </div>
-        `
+      <div class="card">
+        <div class="card-header">${member.name}</div>
+        <div class="card-body">
+         
+          <p><strong>Age:</strong> ${member.age}</p>
+          <p><strong>Retired:</strong> ${member.retired ? 'Yes' : 'No'}</p>
+          <p><strong>Brands:</strong>
+            <ul>
+              ${brandsList}
+            </ul>
+          </p>
+          <p><strong>Best Quality:</strong> ${member.bestQuality}</p>
+          <p><strong>Biography:</strong> ${member.biography}</p>
+        </div>
+      </div>
+    `
 
     teamCardsContainer.appendChild(card)
   })
 }
 
-window.onload = generateTeamCards()
+window.onload = generateTeamCards
